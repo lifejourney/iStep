7482c7e9cb425d162f1ffa682a527f9262d5047a
Steven Zhuang <lifejourney@126.com>
2013-10-16 09:20:14 +0800

    Initial Commit

---
+++ iStep/BWFlipsideViewController.h
@@ -0,0 +1,23 @@
+//
+//  BWFlipsideViewController.h
+//  iStep
+//
+//  Created by steven.zhuang on 10/16/13.
+//  Copyright (c) 2013 steven.zhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BWFlipsideViewController;
+
+@protocol BWFlipsideViewControllerDelegate
+- (void)flipsideViewControllerDidFinish:(BWFlipsideViewController *)controller;
+@end
+
+@interface BWFlipsideViewController : UIViewController
+
+@property (weak, nonatomic) id <BWFlipsideViewControllerDelegate> delegate;
+
+- (IBAction)done:(id)sender;
+
+@end
---
+++ iStepTests/iStepTests.h
@@ -0,0 +1,13 @@
+//
+//  iStepTests.h
+//  iStepTests
+//
+//  Created by steven.zhuang on 10/16/13.
+//  Copyright (c) 2013 steven.zhuang. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface iStepTests : SenTestCase
+
+@end
